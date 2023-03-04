0371fc9cf3f27d50f60c783d4e82e026129a4d3f
paulinamalgorzata <paulinamarucha@gmail.com>
2023-03-04 14:07:30 +0100

    alterations

--- app.js
+++ app.js
@@ -6,5 +6,25 @@ const todoList = document.querySelector(".todo-list");
 todoButton.addEventListener("click", addTodo);
 //Functions
 function addTodo(event) {
-  /* od 16:40 minuty - console log wypróbuj sobie*/
+  // Prevent form from submitting
+  event.preventDefault();
+  //Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  todoDiv.appendChild(newTodo);
+  //CHECK completeButton
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+  //CHECK trashButton
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //append to list
+  todoList.appendChild(todoDiv);
 }
